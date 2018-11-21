e7b9dbfa40c44e8cc5cbab06d71f9e85203f19d1
anhtu <srinnix13@gmail.com>
2018-11-22 06:52:30 +0700

    first commit

---
+++ app/src/main/java/io/srinnix/basekotlin/ui/base/BaseActivity.kt
@@ -0,0 +1,14 @@
+package io.srinnix.basekotlin.ui.base
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class BaseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
+
+
+}
---
+++ app/src/main/java/io/srinnix/basekotlin/ui/base/BaseFragment.kt
@@ -0,0 +1,7 @@
+package io.srinnix.basekotlin.ui.base
+
+import androidx.fragment.app.Fragment
+
+class BaseFragment : Fragment() {
+
+}
